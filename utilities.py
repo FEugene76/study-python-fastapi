463c73c8c8820b4d3a5f897739f3b28b400f0680
FEugene76 <dns877@gmail.com>
2024-11-21 02:40:00 +0200

    Lesson 5

--- utilities.py
+++ utilities.py
@@ -1,17 +1,13 @@
 def calculate_average(numbers):
-    #numbers = [1, 2, 3, 4, 5]  # Пример списка чисел
     if len(numbers) == 0:
         print('Список пустой')
     else:
         result = sum(numbers) / len(numbers)
         return result
-        #print('Среднее арифметическое числа из списка:', average)
 
 def find_max(numbers):
-    #numbers = [1, 2, 3, 4, 5]  # Пример списка чисел
     if len(numbers) == 0:
         print('Список пустой')
     else:
         result = max(numbers)
         return result
-        #print('Максимальное число в списке:', max_number)
